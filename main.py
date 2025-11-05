a4a03851dbef7d4b446dd194e479a8f50119f7e5
EstudianteUC <estudianteUC@UCUXB100209.continental.edu.pe>
2025-11-05 12:03:53 -0500

    cambio

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def saludo (nombre: str ) -> str:
-    return f"hola,{nombre}! bienvenido a Git. commit exitoso"
+    return f"hola,{nombre}! bienvenido a Git. commit exitoso "
 
 if __name__ == "__main__":
     nombre =input("¿tu nombre?")
     print(saludo(nombre))
-    print("test")
